330332fed18db3c3d4ab3be559b81aaab22651d7
Roman Birg <romanbirg@gmail.com>
2013-02-17 02:21:34 -0600

    Toggle rewrite (1/2)
    Change-Id: I80ef0c3f1cbe8e0d35c7a4e4111ad22b96196963
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/drawable/stat_power_background.xml
    	packages/SystemUI/res/layout/quick_settings_tile_generic.xml
    	packages/SystemUI/res/layout/status_bar_expanded.xml
    	packages/SystemUI/res/layout/toggle_tile_user.xml
    	packages/SystemUI/res/values/colors.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
    	packages/SystemUI/src/com/android/systemui/statusbar/tablet/NotificationPanel.java
    
    Conflicts:
    
    	packages/SystemUI/res/values/colors.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/tablet/NotificationPanel.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -117,8 +117,8 @@ import com.android.systemui.statusbar.policy.NetworkController;
 import com.android.systemui.statusbar.policy.NotificationRowLayout;
 import com.android.systemui.statusbar.policy.OnSizeChangedListener;
 import com.android.systemui.statusbar.policy.Prefs;
-import com.android.systemui.statusbar.powerwidget.PowerWidget;
 import com.android.systemui.carbon.CarbonTarget;
+import com.android.systemui.statusbar.toggles.ToggleManager;
 
 import java.io.FileDescriptor;
 import java.io.IOException;
@@ -232,7 +232,7 @@ public class PhoneStatusBar extends BaseStatusBar {
     TextView mNotificationPanelDebugText;
 
     // settings
-    QuickSettings mQS;
+    ToggleManager mToggleManager;
     boolean mHasSettingsPanel, mHasFlipSettings;
     SettingsPanelView mSettingsPanel;
     View mFlipSettingsView;
@@ -863,24 +863,21 @@ public class PhoneStatusBar extends BaseStatusBar {
             // wherever you find it, Quick Settings needs a container to survive
             mSettingsContainer = (QuickSettingsContainerView)
                     mStatusBarWindow.findViewById(R.id.quick_settings_container);
+
+            mToggleManager = new ToggleManager(mContext);
+            mToggleManager.setControllers(mBluetoothController, mNetworkController, mBatteryController,
+                    mLocationController, null);
+            mToggleManager.setContainer((LinearLayout) mNotificationPanel.findViewById(R.id.quick_toggles),
+                    ToggleManager.STYLE_TRADITIONAL);
             if (mSettingsContainer != null) {
-                mQS = new QuickSettings(mContext, mSettingsContainer);
+                mToggleManager.setContainer(mSettingsContainer, ToggleManager.STYLE_TILE);
                 if (!mNotificationPanelIsFullScreenWidth) {
                     mSettingsContainer.setSystemUiVisibility(
                             View.STATUS_BAR_DISABLE_NOTIFICATION_TICKER
-                            | View.STATUS_BAR_DISABLE_SYSTEM_INFO);
+                                    | View.STATUS_BAR_DISABLE_SYSTEM_INFO);
                 }
-                if (mSettingsPanel != null) {
-                    mSettingsPanel.setQuickSettings(mQS);
-                }
-                mQS.setService(this);
-                mQS.setBar(mStatusBarView);
-                mQS.setup(mNetworkController, mBluetoothController, mBatteryController,
-                        mLocationController);
-
-            } else {
-                mQS = null; // fly away, be free
             }
+            mToggleManager.updateSettings();
         }
 
         // Start observing for changes on Notification Drawer (Background & Alpha)
@@ -2105,6 +2102,9 @@ public class PhoneStatusBar extends BaseStatusBar {
     public void flipToSettings() {
         // Settings are not available in setup
         if (!mUserSetup) return;
+        if(mToggleManager != null && !mToggleManager.shouldFlipToSettings()) {
+            return;
+        }
 
         if (mFlipSettingsViewAnim != null) mFlipSettingsViewAnim.cancel();
         if (mScrollViewAnim != null) mScrollViewAnim.cancel();
@@ -2604,7 +2604,6 @@ public class PhoneStatusBar extends BaseStatusBar {
         mCommandQueue.setNavigationIconHints(
                 altBack ? (mNavigationIconHints | StatusBarManager.NAVIGATION_HINT_BACK_ALT)
                         : (mNavigationIconHints & ~StatusBarManager.NAVIGATION_HINT_BACK_ALT));
-        if (mQS != null) mQS.setImeWindowStatus(vis > 0);
     }
 
     @Override
@@ -3099,9 +3098,6 @@ public class PhoneStatusBar extends BaseStatusBar {
                 ((TextView)mClearButton).setText(context.getText(R.string.status_bar_clear_all_button));
             }
 
-            // Update the QuickSettings container
-            if (mQS != null) mQS.updateResources();
-
             loadDimens();
         }
     }
@@ -3354,4 +3350,27 @@ public class PhoneStatusBar extends BaseStatusBar {
         return brightness;
     }
 
+    public boolean skipToSettingsPanel() {
+        if (mPile == null || mNotificationData == null) return false;
+
+        int N = mNotificationData.size();
+        int thisUsersNotifications = 0;
+        for (int i=0; i<N; i++) {
+            Entry ent = mNotificationData.get(N-i-1);
+            if(ent != null
+                    && ent.notification != null
+                    && notificationIsForCurrentUser(ent.notification)) {
+                switch(ent.notification.id) {
+                    // ignore adb icon
+                    case com.android.internal.R.drawable.stat_sys_adb:
+                        continue;
+                }
+                thisUsersNotifications++;
+            }
+        }
+        if(thisUsersNotifications == 0)
+            return true;
+
+        return false;
+    }
 }
